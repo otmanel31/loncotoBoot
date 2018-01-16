ee480a39556d9c10f7016b869b0c32847b89255a
oelkenz <oelkenz@LFR021182.corp.capgemini.com>
2018-01-16 14:20:54 +0100

    first commit using spring boot and finished beans

---
+++ loncontoBoot/src/main/java/com/loncoto/loncontoBoot/LoncontoBootApplication.java
@@ -0,0 +1,12 @@
+package com.loncoto.loncontoBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoncontoBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoncontoBootApplication.class, args);
+	}
+}
